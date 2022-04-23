970e7bd0e39316960bd90abdb8f0d25cb3391105
Benjamin Yim <yan2228598786@gmail.com>
2022-04-23 10:09:11 +0800

    sucess

--- examples/c/tcptop.bpf.c
+++ examples/c/tcptop.bpf.c
@@ -65,18 +65,16 @@ int BPF_KPROBE(tcp_sendmsg,
     __u32 pid = bpf_get_current_pid_tgid() >> 32;
     static const __u64 zero;
     __u64 *val;
-    __u32 family;
-    BPF_CORE_READ_INTO(&family, sk, __sk_common.skc_family);
+    __u32 family = BPF_CORE_READ(sk, __sk_common.skc_family);
 
     if(filter_pid && pid != filter_pid)
     {
         return 0;
     }
-    if(family && family != filter_family)
+    if(filter_family && family != filter_family)
     {
         return 0;
     }
-
     if(family == AF_INET)
     {
         struct ipv4_key_t ipv4_key = {
@@ -84,12 +82,13 @@ int BPF_KPROBE(tcp_sendmsg,
         };
         bpf_get_current_comm(&ipv4_key.name, sizeof(ipv4_key.name));
         BPF_CORE_READ_INTO(&ipv4_key.laddr, sk, __sk_common.skc_rcv_saddr);
-        BPF_CORE_READ_INTO(&ipv4_key.daddr, sk, __sk_common.skc_daddr);
-        BPF_CORE_READ_INTO(&ipv4_key.lport, sk, __sk_common.skc_num);
-        BPF_CORE_READ_INTO(&ipv4_key.dport, sk, __sk_common.skc_dport);
+        ipv4_key.daddr = BPF_CORE_READ(sk, __sk_common.skc_daddr);
+        ipv4_key.lport = BPF_CORE_READ(sk, __sk_common.skc_num);
+        ipv4_key.dport = BPF_CORE_READ(sk, __sk_common.skc_dport);
         val = bpf_map_lookup_or_try_init(&ipv4_send_bytes,&ipv4_key,&zero);
         if (val) {
-            __atomic_add_fetch(val, 1, __ATOMIC_RELAXED);
+            __atomic_add_fetch(val, size, __ATOMIC_RELAXED);
+            bpf_printk("AF_INET %lld\n",ipv4_key.laddr);
         }
     } else if(family == AF_INET6)
     {
@@ -103,7 +102,62 @@ int BPF_KPROBE(tcp_sendmsg,
         BPF_CORE_READ_INTO(&ipv6_key.dport, sk, __sk_common.skc_dport);
         val = bpf_map_lookup_or_try_init(&ipv6_send_bytes,&ipv6_key,&zero);
         if (val) {
-            __atomic_add_fetch(val, 1, __ATOMIC_RELAXED);
+            __atomic_add_fetch(val, size, __ATOMIC_RELAXED);
+        }
+    }
+    return 0;
+}
+
+
+SEC("kprobe/tcp_cleanup_rbuf")
+int BPF_KPROBE(tcp_cleanup_rbuf,
+               struct sock *sk, int copid)
+{
+    __u32 pid = bpf_get_current_pid_tgid() >> 32;
+    static const __u64 zero;
+    __u64 *val;
+    __u32 family = BPF_CORE_READ(sk, __sk_common.skc_family);
+
+    if(filter_pid && pid != filter_pid)
+    {
+        return 0;
+    }
+    if(filter_family && family != filter_family)
+    {
+        return 0;
+    }
+    if(copid <= 0)
+    {
+        return 0;
+    }
+    if(family == AF_INET)
+    {
+        struct ipv4_key_t ipv4_key = {
+            .pid = pid
+        };
+        bpf_get_current_comm(&ipv4_key.name, sizeof(ipv4_key.name));
+        BPF_CORE_READ_INTO(&ipv4_key.laddr, sk, __sk_common.skc_rcv_saddr);
+        ipv4_key.daddr = BPF_CORE_READ(sk, __sk_common.skc_daddr);
+        ipv4_key.lport = BPF_CORE_READ(sk, __sk_common.skc_num);
+        ipv4_key.dport = BPF_CORE_READ(sk, __sk_common.skc_dport);
+        val = bpf_map_lookup_or_try_init(&ipv4_recv_bytes, &ipv4_key, &zero);
+        if (val) {
+            __atomic_add_fetch(val, copid, __ATOMIC_RELAXED);
+            bpf_printk("AF_INET %lld\n",ipv4_key.laddr);
+        }
+    } else if(family == AF_INET6)
+    {
+        struct ipv6_key_t ipv6_key = {
+            .pid = pid
+        };
+        bpf_get_current_comm(&ipv6_key.name, sizeof(ipv6_key.name));
+        BPF_CORE_READ_INTO(&ipv6_key.laddr, sk, __sk_common.skc_v6_rcv_saddr.in6_u.u6_addr32);
+        BPF_CORE_READ_INTO(&ipv6_key.daddr, sk, __sk_common.skc_v6_daddr.in6_u.u6_addr32);
+        BPF_CORE_READ_INTO(&ipv6_key.lport, sk, __sk_common.skc_num);
+        BPF_CORE_READ_INTO(&ipv6_key.dport, sk, __sk_common.skc_dport);
+        val = bpf_map_lookup_or_try_init(&ipv6_recv_bytes, &ipv6_key, &zero);
+        if (val) {
+            __atomic_add_fetch(val, copid, __ATOMIC_RELAXED);
         }
     }
     return 0;
--- examples/c/tcptop.h
+++ examples/c/tcptop.h
@@ -28,15 +28,4 @@ struct ipv4_key_t {
     __u16 lport;
     __u16 dport;
 };
-
-
-struct ipv6_key_t {
-    __u32 pid;
-    char name[TASK_COMM_LEN];
-    __u32 laddr;
-    __u32 daddr;
-    __u16 lport;
-    __u16 dport;
-};
-
 #endif
